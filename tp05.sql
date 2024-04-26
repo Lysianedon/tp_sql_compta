cfd7ffc898f16f55230f2178e2fa3f1122be00c4
lysiandon <don.lysiane@gmail.com>
2024-04-26 09:53:21 +0200

    working on part 2

--- tp05.sql
+++ tp05.sql
@@ -1,8 +1,24 @@
--- List all articles ordered by designation in ascending order:
+--a. List all articles ordered by designation in ascending order:
 SELECT * FROM ARTICLE ORDER BY designation ASC;
 
--- List all articles ordered by price in descending order:
+--b. List all articles ordered by price in descending order:
 SELECT * FROM ARTICLE ORDER BY prix DESC;
 
--- List all articles which are bolt, ordered by price in ascending order:
+--c. List all articles which are bolt, ordered by price in ascending order:
 SELECT * FROM ARTICLE WHERE DESIGNATION LIKE '%boulon%' ORDER BY prix ASC;
+
+-- d. List all articles which designation contain the word 'sachet':
+SELECT * FROM ARTICLE WHERE DESIGNATION LIKE '%sachet%';
+
+-- e. List all articles which designation contain the word 'sachet'. The request must be case insensitive: INFO => Since the encoding type of the database is utf_general_ci, the sorting is already case insensitive, so we dont need to specify anything about the case:
+SELECT * FROM ARTICLE WHERE DESIGNATION LIKE '%sachet%'; 
+
+-- f. List all articles with their corresponding supplier information. The results must be sorted alphabetically by the suppliers' names and then by price in descending order:
+SELECT * FROM ARTICLE LEFT JOIN FOURNISSEUR ON ARTICLE.ID_FOU = FOURNISSEUR.ID ORDER BY FOURNISSEUR.NOM ASC, PRIX DESC;
+
+-- g. List all articles from 'Dubois & Fils' company:
+SELECT ARTICLE.* FROM ARTICLE LEFT JOIN FOURNISSEUR ON ARTICLE.ID_FOU = FOURNISSEUR.ID 
+WHERE FOURNISSEUR.NOM = 'Dubois & Fils';
+
+
+
